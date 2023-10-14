f52c0340ea82eb1628b5707e311aeb96b5852097
smokingplaya <smokingplaya@ya.ru>
2023-10-14 23:58:16 +0300

    (update) shared function loadpkg; clientside packages

--- lua/corium/modules/package_manager.lua
+++ lua/corium/modules/package_manager.lua
@@ -1,10 +1,8 @@
-print("S")
-
 function loadpkg(name) -- you can use this function on clientside
   if not name then error("There is no argument") end
-  if not filesystem.Exists("lua/cpm/" .. name .. ".lua") then error("Package \"" .. name .. "\n not installed.") end
+  if not file.Exists("cpm/" .. name .. ".lua", "LUA") then error("Package \"" .. name .. "\n not installed.") end
 
-  local args = {include("cpm")}
+  local args = {include("cpm/" .. name .. ".lua")}
 
   if #args == 0 then error("\"" .. name .. "\"library did not return the arguments.") end
 
@@ -15,10 +13,13 @@ if not SERVER then return end
 
 require("gm_filesystem")
 
-local function get_in_db(name) local query = sql.Query("SELECT * FROM cpm WHERE name = " .. SQLStr(name)) return istable(query) and query[1] or query end
-
 local github_repo = "gmodium/packages"
 local sql_table = "cpm"
+local function get_in_db(name)
+  local query = sql.Query("SELECT * FROM " .. sql_table .. " WHERE name = " .. SQLStr(name))
+
+  return istable(query) and query[1] or query
+end
 
 if not filesystem.Exists("lua/cpm") then
   filesystem.DirCreate("lua/cpm")
@@ -33,12 +34,15 @@ end
 local files = file.Find("cpm/*.lua", "LUA")
 
 for _, name in ipairs(files) do
-  local d = get_in_db(name)
+  local d = get_in_db(name:Left(#name-4))
 
-  if d.allow_cl then
-    AddCSLuaFile("cpm/" .. name .. ".lua")
+  if not d then continue end
+
+  if d.allow_cl == "1" then
+    AddCSLuaFile("cpm/" .. name)
   end
 end
+
 
 --
 
@@ -49,7 +53,7 @@ Corium.commands.Add("install", function(args)
   http.Fetch("https://raw.githubusercontent.com/" .. github_repo .. "/" .. (args[2] or "main") .. "/" .. name .. "/lib.lua", function(b)
     http.Fetch("https://raw.githubusercontent.com/" .. github_repo .. "/" .. (args[2] or "main") .. "/" .. name .. "/lib.json", function(json_b)
       local lib_info = util.JSONToTable(json_b)
-      local allow_cl = lib_info.allow_clientside
+      local allow_cl = lib_info.allow_clientside and lib_info.allow_clientside == 1
       local path = "lua/cpm/" .. name .. ".lua"
 
       if filesystem.Exists(path) then
@@ -58,14 +62,12 @@ Corium.commands.Add("install", function(args)
   
       filesystem.Create(path, b)
 
-      if allow_clientside then
+      if allow_cl then
         AddCSLuaFile("cpm/" .. name .. ".lua")
       end
-
-      include("cpm/" .. name .. ".lua")
   
       if not get_in_db(name) then
-        sql.Query("INSERT INTO cpm(name, allow_cl) VALUES(" .. SQLStr(name).. ", " .. (allow_clientside and 1 or 0) .. ")")
+        sql.Query("INSERT INTO cpm(name, allow_cl) VALUES(" .. SQLStr(name).. ", " .. (allow_cl and 1 or 0) .. ")")
       end
     end, function(msg)
       console_log("Unable to install package \"" .. name .. "\", server response: " .. (msg "none"))
